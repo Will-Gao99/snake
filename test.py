00ce5aab6cd4c2d9b184ca8abdc9725df2ba78bf
Will-Gao99 <wg253@cornell.edu>
2020-01-02 22:41:59 -0500

    Added tests for Node. Still need to add tests for linkedlist

--- test.py
+++ test.py
@@ -2,14 +2,39 @@ import script
 import linkedlist
 
 def node_tests():
-    pass
+    print("Testing class Node")
+    # Testing constructor
+    init_node = linkedlist.Node(1)
+    assert init_node.val == 1, "value should be 1"
+    assert init_node.next == None, "next should be None"
+
+    # Testing set_next
+    next_node = linkedlist.Node(2)
+    init_node.set_next(next_node)
+    assert init_node.next == next_node, "next should be next_node"
+
+    # Testing get_next
+    assert init_node.get_next() == next_node, "get_next should return next_node"
+
+    # Testing set_val
+    next_node.set_val(3)
+    assert next_node.val == 3, "value should be set to 3"
+
+    # Testing get_val
+    assert next_node.get_val() == 3, "get_val should return 3"
+
+    # Testing has_next
+    assert init_node.has_next() == True, "init_node should have a succesor"
+    assert next_node.has_next() == False, "next_node should not have a succesor"
+
+    print("This module has passed its tests")
 
 def linkedlist_tests():
     pass
 
 def piece_tests():
     print("Testing class Piece")
-    # Testing initializer
+    # Testing constructor
     init_piece = script.Piece(0,0)
     assert init_piece.x == 0, "x coordinate should be 0"
     assert init_piece.y == 0, "y coordinate should be 0"
@@ -25,9 +50,11 @@ def piece_tests():
     # Testing to_string
     assert init_piece.to_str() == "[(0,0), n=1, right, velocity=20]", "string representation incorrect"
 
+    print("This module has passed its tests")
+
 def snake_tests():
     print("Testing class Snake")
-    # Testing initializer
+    # Testing constructor
     init_snake = script.Snake(4,0,0)
     assert init_snake.x == 0, "x coordinate should be 0"
     assert init_snake.y == 0, "y cooridnate should be 0"
@@ -48,6 +75,8 @@ def snake_tests():
     #moved_snake.build()
     #moved_snake.move()
     #assert moved_snake.x == (400 + moved_snake.pieces[0].vel), "snake hasn't moved properly"
+
+    print("This module has passed its tests")
 
 def main():
     node_tests()
